3a6c2861d60a1d4c174acc41a5684ae62716a459
jola-blvhk <awujoolabello@gmail.com>
2023-09-19 11:51:22 +0100

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,13 +8,7 @@ export const Header = () => {
     <nav className="flex-center fixed top-0 z-50 w-full border-b-2 border-black-200 py-7 text-white bg-black-100">
       <div className="flex-between mx-auto w-full max-w-screen-2xl px-6 xs:px-8 sm:px-16">
         <Link href="/">
-          <Image
-            src="/tv.png"
-            alt="Next.js logo"
-            width={50}
-            height={50}
-            className="cursor-pointer"
-          />
+         <h1 className="">get<span className=" text-primary-pink_100">linked</span></h1>
         </Link>
 
         <div>
