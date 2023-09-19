8d8abf4a2921f0916089568706be603192d543b2
jola-blvhk <awujoolabello@gmail.com>
2023-09-19 11:54:03 +0100

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,15 +5,15 @@ import Link from "next/link";
 
 export const Header = () => {
   return (
-    <nav className="flex-center fixed top-0 z-50 w-full border-b-2 border-black-200 py-7 text-white bg-black-100">
+    <nav className="flex-center fixed top-0 z-50 w-full border-b-2 border-black-200 py-7 text-white bg-primary-purple_100">
       <div className="flex-between mx-auto w-full max-w-screen-2xl px-6 xs:px-8 sm:px-16">
         <Link href="/">
-         <h1 className="">get<span className=" text-primary-pink_100">linked</span></h1>
+          <h1 className="font-bold text-2xl md: text-3xl">
+            get<span className=" text-primary-pink_100">linked</span>
+          </h1>
         </Link>
 
-        <div>
-          {/* <Input className="" /> */}
-        </div>
+        <div>{/* <Input className="" /> */}</div>
 
         {/* <Menu className="block md:hidden" /> */}
 
@@ -33,5 +33,3 @@ export const Header = () => {
     </nav>
   );
 };
-
-
