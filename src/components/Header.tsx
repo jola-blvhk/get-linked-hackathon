f24cbdd8512204766a1e4199bb1467bf13cfd6d3
Richard Emijere <67247138+The-CodeINN@users.noreply.github.com>
2023-09-21 10:13:56 +0000

    Worked on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,39 +1,39 @@
 // import { Menu } from "lucide-react";
-import Image from "next/image";
-import Link from "next/link";
-import { Button } from ".";
-import MobileNav from "./MobileNav";
+import Image from 'next/image';
+import Link from 'next/link';
+import { Button } from '.';
+import MobileNav from './MobileNav';
 // import { Input } from "./ui/input";
 
 export const Header = () => {
   return (
-    <nav className=" fixed top-0 z-50 w-full border-b-2 border-white border-opacity-25 py-7 bg-transparent text-white">
-      <div className="flex-between  w-full  padding_section">
-        <Link href="/">
-          <h1 className="font-bold text-2xl md:text-3xl font-heading">
-            get<span className=" text-primary-pink_100">linked</span>
+    <nav className=' fixed top-0 z-50 w-full border-b-2 border-white border-opacity-25 py-7 bg-transparent text-white'>
+      <div className='flex-between  w-full padding_section'>
+        <Link href='/'>
+          <h1 className='font-bold text-2xl md:text-3xl font-heading'>
+            get<span className=' text-primary-pink_100'>linked</span>
           </h1>
         </Link>
 
-        <div className="flex-center max-md:hidden md:space-x-20">
-          <ul className="flex-center gap-x-3 md:gap-x-10">
-            <li className="font-bold hover:text-gradient_blue-purple">
-              <Link href="/" target="_blank">
+        <div className='flex-center md:hidden md:space-x-20'>
+          <ul className='flex-center gap-x-3 md:gap-x-3'>
+            <li className='font-bold hover:text-gradient_blue-purple'>
+              <Link href='/' target='_blank'>
                 Timeline
               </Link>
             </li>
-            <li className="hover:text-gradient_blue-purple !font-bold">
-              <Link href="/" target="_blank">
+            <li className='hover:text-gradient_blue-purple !font-bold'>
+              <Link href='/' target='_blank'>
                 Overview
               </Link>
             </li>
-            <li className="body-text hover:text-gradient_blue-purple !font-bold">
-              <Link href="/" target="_blank">
+            <li className='hover:text-gradient_blue-purple !font-bold'>
+              <Link href='/' target='_blank'>
                 Faqs
               </Link>
             </li>
-            <li className="body-text hover:text-gradient_blue-purple !font-bold">
-              <Link href="/" target="_blank">
+            <li className='hover:text-gradient_blue-purple !font-bold'>
+              <Link href='/' target='_blank'>
                 Contact
               </Link>
             </li>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,39 +1,40 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     fontFamily: {
-      heading: "var(--heading-font)",
-      body: "var(--body-font)",
-      unica: "var(--unica-font)",
+      heading: 'var(--heading-font)',
+      body: 'var(--body-font)',
+      unica: 'var(--unica-font)',
     },
     extend: {
       screens: {
-        xs: "400px",
-        sm: "640px",
-        md: "768px",
-        lg: "1024px",
-        xl: "1220px",
-        "2xl": "1440px",
-        "3xl": "1700px",
+        xs: '400px',
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1220px',
+        '2xl': '1440px',
+        '3xl': '1700px',
+        'custom-1': '1920px',
       },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-pink": "linear-gradient(to right,#FE34B9, #903AFF )",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-pink': 'linear-gradient(to right,#FE34B9, #903AFF )',
         section_one: "url('/assets/Section_One_Gradient.png')",
         section_one_mobile: "url('/assets/section-one-mobile-gradient.png')",
       },
       colors: {
         primary: {
-          pink_100: "#D434FE",
-          purple_100: "#903AFF",
-          dark_purple: "#150E28",
-          gradient_pink: "linear-gradient(to right,#FE34B9, #903AFF )",
+          pink_100: '#D434FE',
+          purple_100: '#903AFF',
+          dark_purple: '#150E28',
+          gradient_pink: 'linear-gradient(to right,#FE34B9, #903AFF )',
         },
       },
     },
